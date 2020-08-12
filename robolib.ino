bfc9b10d28552b0967a87a483ab32784e356a8bb
Julen Ortiz <5983309+Julortiz@users.noreply.github.com>
2020-08-12 12:51:06 +0200

    Configurazio aukerak gehitu

--- robolib.ino
+++ robolib.ino
@@ -2,13 +2,23 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+//------------Konfigurazioaren hasiera-----------------
+aurrera_balioa = 180
+atzera_balioa = 0
+gelditu_balioa = 90
+  
+pin_ezkerreko_motorra = 7
+pin_eskubiko_motorra = 8
+
+//------------Konfigurazioaren bukaera-----------------
+
 Servo ezkerreko_motorra;
 Servo eskuineko_motorra;
 
 void gelditu()
 {
-  ezkerreko_motorra.write(90);
-  eskuineko_motorra.write(90);
+  ezkerreko_motorra.write(gelditu_balioa);
+  eskuineko_motorra.write(gelditu_balioa);
 }
 
 void itxaron(float duration)
@@ -18,40 +28,40 @@ void itxaron(float duration)
 
 void atzera(float duration)
 {
-  ezkerreko_motorra.write(0);
-  eskuineko_motorra.write(180);
+  ezkerreko_motorra.write(atzera_balioa);
+  eskuineko_motorra.write(aurrera_balioa);
   itxaron(duration);
   gelditu();
 }
 
 void aurrera(float duration)
 {
-  ezkerreko_motorra.write(180);
-  eskuineko_motorra.write(0);
+  ezkerreko_motorra.write(aurrera_balioa);
+  eskuineko_motorra.write(atzera_balioa);
   itxaron(duration);
   gelditu();
 }
 
 void ezkerrera(float duration)
 {
-  ezkerreko_motorra.write(0);
-  eskuineko_motorra.write(0);
+  ezkerreko_motorra.write(atzera_balioa);
+  eskuineko_motorra.write(atzera_balioa);
   itxaron(duration);
   gelditu();
 }
 
 void eskubira(float duration)
 {
-  ezkerreko_motorra.write(180);
-  eskuineko_motorra.write(180);
+  ezkerreko_motorra.write(aurrera_balioa);
+  eskuineko_motorra.write(aurrera_balioa);
   itxaron(duration);
   gelditu();
 }
 
 void giratu(int graduak)
 {
-  ezkerreko_motorra.write(0);
-  eskuineko_motorra.write(0);
+  ezkerreko_motorra.write(atzera_balioa);
+  eskuineko_motorra.write(atzera_balioa);
   itxaron(1);
   gelditu();
 }
@@ -62,6 +72,6 @@ void bukatu(){
 
 void hasieratu()
 {
-  ezkerreko_motorra.attach(7);
-  eskuineko_motorra.attach(8);
+  ezkerreko_motorra.attach(pin_ezkerreko_motorra);
+  eskuineko_motorra.attach(pin_eskubiko_motorra);
 }
